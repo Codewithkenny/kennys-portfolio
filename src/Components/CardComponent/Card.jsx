f4066cd4e4547eb08f293bcca934baf85f2bd46d
Olapoju Kenny Agbomeji <pojuagbomeji@gmail.com>
2023-12-24 02:42:33 +0100

    updated the p-tag portfolio in the card

--- src/Components/CardComponent/Card.jsx
+++ src/Components/CardComponent/Card.jsx
@@ -10,10 +10,7 @@ export const Card = () => {
       <div className="pl-text">
         <h1 className="pl-title">My Projects</h1>
         <p className="pl-desc">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam illo
-          repellat animi, sapiente temporibus delectus magnam maiores
-          repudiandae aliquid? Molestiae quo beatae repellat magni placeat
-          incidunt officia? Eos, modi fugit.
+          This project section showcases a few projects i emabarked on, each highlighting my skills in web development and problem-solving. From developing an online food delivery system to creating a user-friendly e-commerce platform, I have demonstrated my ability to work with a variety of technologies and deliver successful projects. Each project showcases my attention to detail, creativity, and drive to exceed client expectations. I am confident that these projects demonstrate my ability to turn complex problems into effective solutions.
         </p>
       </div>
       <div className="pl-list">
